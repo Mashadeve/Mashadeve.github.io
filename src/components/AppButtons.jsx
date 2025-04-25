13e4ff66e95a833e74d35eb7df52c8769216b660
Mashadeve <maenpaa.nk@gmail.com>
2025-04-25 14:29:49 +0300

    Toasti lisätty

--- src/components/AppButtons.jsx
+++ src/components/AppButtons.jsx
@@ -1,9 +1,18 @@
-
+import { toast } from 'react-toastify';
 
 const AppButtons = () => {
+
+    const handleClick = () =>{
+        toast.success('Patience young padawan... There will be content...', {
+            position: 'top-center',
+            autoClose:3000,
+            theme: 'dark',
+        });
+    };
+
     return (
         <div className="flex justify-center gap-4">
-            <button className="px-4 py-2 text-white text-lg sm:text-xl font-pixel bg-[#181818] hover:shadow-white-outline rounded-sm">Nearest App</button>
+            <button className="px-4 py-2 text-white text-lg sm:text-xl font-pixel bg-[#181818] hover:shadow-white-outline rounded-sm" onClick={handleClick}>Nearest App</button>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Toastifyn tyylit
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      closeOnClick
+      pauseOnHover
+      draggable
+      theme="dark"
+    />
     <App />
   </StrictMode>,
 )
