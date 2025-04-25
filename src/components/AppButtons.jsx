ab279e2fa86c5279d588f541ea7a995d66fa73f8
Mashadeve <maenpaa.nk@gmail.com>
2025-04-25 13:29:59 +0300

    puskua luvassa

--- src/components/AppButtons.jsx
+++ src/components/AppButtons.jsx
@@ -3,7 +3,7 @@
 const AppButtons = () => {
     return (
         <div className="flex justify-center gap-4">
-            <button className="px-4 py-2 text-white text-lg sm:text-xl font-pixel bg-[#181818] hover:shadow-white-outline rounded-sm">Nearest App</button>
+            <button className="px-4 py-2 text-white text-lg sm:text-xl font-pixel bg-[#181818] hover:shadow-white-outline rounded-sm">Nearest App</button> //nappi
         </div>
     );
 };
